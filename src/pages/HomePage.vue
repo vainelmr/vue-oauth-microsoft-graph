078325277b9f151c082cb413e401e488337cd103
vainelmr <lemairevaine@gmail.com>
2025-04-18 11:07:00 +0200

    Ex 4: create HomePage component

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <h1>Welcome to the Homepage</h1>
+        <p>This is the homepage of our application.</p>
+    </div>
+</template>
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'HomePage'
+}
+</script>
+<style lang="">
+    
+</style>
